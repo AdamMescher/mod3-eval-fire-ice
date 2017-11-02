7ecd66cef042af1ccce5f69c9bb3d30e2de54546
Adam Mescher <admescher@gmail.com>
2017-11-02 10:19:58 -0600

    Add house card data when house card is rendered

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,37 +4,39 @@ import logo from './logo.svg';
 import { connect } from 'react-redux';
 import { fakeAction } from '../../actions';
 import HouseCard from '../HouseCard/HouseCard';
-import 
 
 
 class App extends Component {
 
   componentDidMount(){ 
-    console.log(props);
-    // fetch(`http://localhost:3001/api/v1/houses`)
-    //   .then( response => response.json() )
-    //   .then( parsed => Promise.all(parsed) )
+    fetch(`http://localhost:3001/api/v1/houses`)
+      .then( response => response.json() )
+      .then( parsed => Promise.all(parsed) )
+      .then(resolvedHouseDataArrayPromises => this.props.setHouseData(resolvedHouseDataArrayPromises) )
   }
 
   render() {
+
+    const mappedHouseData = this.props.houseData.map( house => <HouseCard houseData={house}/> )
     
-    
-    return (
-        <div className='App'>
-          <div className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <h2>Welcome to Westeros</h2>
-            <button onClick={() => {
-              this.props.fakeAction();
-              alert(this.props.fake);
-            }}> FAKE ACTION</button>
-          </div>
-          <div className='Display-info'>
-          </div>
+    console.log('RENERED');
+  return (
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to Westeros</h2>
+          <button onClick={() => {
+            this.props.fakeAction();
+            alert(this.props.fake);
+          }}> FAKE ACTION</button>
         </div>
-      );
-    }
+        <div className='Display-info'>
+          {mappedHouseData}
+        </div>
+      </div>
+    );
   }
+}
 
 App.propTypes = {
   fake: shape({ fake: string }),
